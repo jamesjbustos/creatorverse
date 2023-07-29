d28a0bcb35891b8ca7ff8b8401029e337f6a9398
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-07-29 17:00:32 -0400

    Add components and pages

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <h2>name</h2>
+      <a href="#">url</a>
+      <p>creator description</p>
+    </div>
+  );
+};
+
+export default Card;
