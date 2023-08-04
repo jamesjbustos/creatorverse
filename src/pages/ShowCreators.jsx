5f484c6fcc065b832afed94301f68ff25cfef25d
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-08-04 10:39:24 -0400

    Implemented ShowCreator Page

--- src/pages/ShowCreators.jsx
+++ src/pages/ShowCreators.jsx
@@ -1,5 +1,30 @@
-const ShowCreators = () => {
-  return <div>ShowCreators</div>;
+import CreatorCard from "../components/CreatorCard";
+
+const ShowCreators = ({ creators }) => {
+  return (
+    <div>
+      <h1 className="ShowCreators-title">🎨 Creators</h1>
+      <section className="ShowCreators">
+        {/* Check if there are creators */}
+        {creators.length > 0 ? (
+          creators.map((creator) => (
+            // If creators exist, map over the array and render a CreatorCard.
+            <CreatorCard
+              key={creator.id} // Unique creator id from database
+              id={creator.id}
+              name={creator.name}
+              url={creator.url}
+              description={creator.description}
+              imageURL={creator.imageURL}
+            />
+          ))
+        ) : (
+          // If there are no creators, display a message
+          <p>😢 No content creators found.</p>
+        )}
+      </section>
+    </div>
+  );
 };
 
 export default ShowCreators;
