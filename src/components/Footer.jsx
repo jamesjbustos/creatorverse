444f6a86e429f3f8e18fa1692a749cba4432a97e
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-08-07 13:57:05 -0400

    Add github link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+
 const Footer = () => {
   return (
     <footer className="Footer">
-      <p className="Footer-title">💫 creatorverse</p>
+      <div className="Footer-container">
+        <p className="Footer-title">💫 creatorverse</p>
+        <span className="Footer-dot">·</span>
+        <a
+          href="https://github.com/jamesjbustos/creatorverse"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHubIcon className="Footer-icon" fontSize="small" />
+        </a>
+      </div>
     </footer>
   );
 };
